ddcb2e33898e6de4a1f1646e478a982f8739d2c4
matteo-incicco <matteoincicco@gmail.com>
2024-10-31 15:16:21 +0100

    .

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -2,10 +2,12 @@ from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.slack.notifications.slack_notifier import SlackNotifier
 from datetime import datetime
 import requests
 
-from include.stock_market.tasks import _get_stock_prices
+from include.stock_market.tasks import _get_stock_prices, _store_prices
 
 SYMBOL = 'AAPL'
 
@@ -13,7 +15,17 @@ SYMBOL = 'AAPL'
     start_date=datetime(2024, 1, 1),
     schedule="@daily",
     catchup=False,
-    tags=["stock_market"]
+    tags=["stock_market"],
+    on_success_callback=SlackNotifier(
+        slack_conn_id='slack',
+        text='The DAG stock_market has succeded',
+        channel='generale'
+    ),
+    on_failure_callback=SlackNotifier(
+        slack_conn_id='slack',
+        text='The DAG stock_market has failed',
+        channel='generale'
+    )
 )
 def stock_market():
 
@@ -28,9 +40,31 @@ def stock_market():
     get_stock_prices = PythonOperator(
         task_id='get_stock_prices',
         python_callable=_get_stock_prices,
-        op_kwargs={'url': '{{ task_instance.xcom_pull(task_ids="is_api_available")}}', 'simbol': SYMBOL} # with the double { we say that te value should be replaced at runtime by airflow
+        op_kwargs={'url': '{{ task_instance.xcom_pull(task_ids="is_api_available")}}', 'symbol': SYMBOL} # with the double { we say that te value should be replaced at runtime by airflow
     )
 
-    is_api_available()
+    store_prices = PythonOperator(
+        task_id='store_prices',
+        python_callable=_store_prices,
+        op_kwargs={'stock': '{{ task_instance.xcom_pull(task_ids="get_stock_prices")}}'}
+    )
+
+    format_prices = DockerOperator(
+        task_id='format_prices',
+        image='airflow/spark-app',
+        container_name= 'format_prices',
+        api_version='auto',
+        auto_remove=True,
+        docker_url='tcp://docker-proxy:2375',
+        network_mode='container:spark-master',
+        tty=True,
+        xcom_all=False,
+        mount_tmp_dir=False,
+        environment={
+            'SPARK_APPLICATION_ARGS': '{{ task_instance.xcom_pull(task_ids="store_prices") }}'
+        }
+    )
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices
 
 stock_market()
--- include/stock_market/tasks.py
+++ include/stock_market/tasks.py
@@ -1,8 +1,61 @@
 from airflow.hooks.base import BaseHook
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import requests
+import json
+from io import BytesIO
 
 def _get_stock_prices(url, symbol):
     url = f"{url}{symbol}?metrics=high?&interval=1d&range=1y"
     api = BaseHook.get_connection('stock_api')
     response = requests.get(url, headers=api.extra_dejson['headers'])
     return json.dumps(response.json()['chart']['result'][0])
+
+def _store_prices(stock):
+    # Parse the stock data if it's a JSON string
+    if isinstance(stock, str):
+        stock = json.loads(stock)
+
+    # Set up the S3 connection using the S3Hook
+    s3_hook = S3Hook(aws_conn_id='aws_s3')
+    bucket_name = 'codingmatte'  # Root bucket name
+    
+    # Prepare the full path and file structure
+    symbol = stock['meta']['symbol']
+    s3_key = f'data/ingestion/airflow_stock_prices/{symbol}/prices.json'
+
+    # Prepare data to upload
+    data = json.dumps(stock, ensure_ascii=False).encode('utf8')
+    
+    # Upload data to S3
+    s3_hook.load_bytes(
+        bytes_data=BytesIO(data).read(),  # Load data as bytes
+        key=s3_key,                       # S3 key with desired path
+        bucket_name=bucket_name,
+        replace=True                      # Overwrite if file exists
+    )
+
+    # Return the S3 path of the stored object
+    return f's3://{bucket_name}/{s3_key}'
+
+def _store_prices_minio(stock):
+    minio = BaseHook.get_connection('minio').extra_dejson
+    client = Minio(
+        endpoint=minio['endpoint_url'].split('//')[1],
+        access_key=minio['aws_access_key_id'],
+        secret_key=minio['aws_secret_access_key'],
+        secure=False
+    )
+    bucket_name = 'stock-market'
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+    symbol = stock['meta']['symbol']
+    data = json.dumps(stock, ensure_ascii=False).encode('utf8')
+
+    objw = client.put_object(
+        bucket_name=bucket_name,
+        object_name=f'{symbol}/prices.json',
+        data=BytesIO(data),
+        length=len(data)
+    )
+    return f'{objw.bucket_name}/{symbol}'
+    
